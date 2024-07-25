989717a07ae0b8f0b09daf50ad9d74da8fa65df9
williammasivi <143694766+williammasivi@users.noreply.github.com>
2024-07-25 11:40:53 +0200

    Delete client directory

--- client/src/pages/Pharmacies.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PharmacyLocator from '../components/PharmacyLocator';
-
-const Pharmacies = () => {
-  return <PharmacyLocator />;
-};
-
-export default Pharmacies;
--- client/src/pages/Records.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MedicalRecords from '../components/MedicalRecords';
-
-const Records = () => {
-  return <MedicalRecords />;
-};
-
-export default Records;
